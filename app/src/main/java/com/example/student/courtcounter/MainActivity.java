b3ea5c4523dd9a1186e85c235b78ac7fb3f100ee
Jesse Chi-Su-Gutierrez <jchisu31@gmail.com>
2016-10-24 14:17:14 -0700

    Second Commit
    More code and milestones.

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.student.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView countView;
+    public int scoreOne = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countView= (TextView)findViewById(R.id.buttonCounter);
+        countView.setText(scoreOne);
+
+    }
+
+    public void clickFunction1(View view){
+        countView.setText(String.valueOf(scoreOne));
+        scoreOne++;
     }
 }
